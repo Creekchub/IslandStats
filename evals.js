511a4c25646d01154ece7db8dd7f1bd95e93d3c4
Creekchub <139086933+Creekchub@users.noreply.github.com>
2023-07-26 17:07:11 -0400

    Update evals.js

--- evals.js
+++ evals.js
@@ -1,7 +1,5 @@
 let k = 'value';
 
 let games = ['sb', 'bb', 'hitw', 'tgttos'];
-for (i = 0; i < 3; i++) {
-    eval('var ' + games[i] + 'coins' + '= ' + gameSel + ';');
-    console.log(eval(games[i] + 'coins'));
-}
+    eval('var ' + games[0] + 'coins' + '= ' + 'games[0]' + ';');
+    console.log(sbcoins);
