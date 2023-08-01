c071af427b6f7aa18761871dfac28b68f0a3626c
Creekchub <139086933+Creekchub@users.noreply.github.com>
2023-08-01 12:59:23 -0400

    d
    d

---
+++ players.js
@@ -0,0 +1,1132 @@
+let players = {
+  Oceanseaas: {
+    iw: {
+      2: {
+        sb: 323,
+        tgttos: 862,
+        bb: 1218,
+        hitw: 543,
+      }
+    }
+  },
+  Shadowsz: {
+    iw: {
+      2: {
+        sb: 40,
+        tgttos: 1505,
+        bb: 1440,
+        hitw: 669,
+      }
+    }
+  },
+  MattySP: {
+    iw: {
+      2: {
+        sb: 48,
+        tgttos: 1212,
+        bb: 1084,
+        hitw: 664,
+      }
+    }
+  },
+  Cbop777: {
+    iw: {
+      2: {
+        sb: 143,
+        tgttos: 1000,
+        bb: 994,
+        hitw: 713,
+      }
+    }
+  },
+  LargeEpicMan: {
+    iw: {
+      2: {
+        sb: 279,
+        tgttos: 561,
+        bb: 891,
+        hitw: 873,
+      }
+    }
+  },
+  Fawnardvark: {
+    iw: {
+      2: {
+        sb: 374,
+        tgttos: 952,
+        bb: 1531,
+        hitw: 678,
+        sub: true
+      }
+    }
+  },
+  Yumalol: {
+    iw: {
+      2: {
+        sb: 374,
+        tgttos: 952,
+        bb: 1531,
+        hitw: 678,
+        sub: true
+      }
+    }
+  },
+  Mellojellocello: {
+    iw: {
+      2: {
+        sb: 402,
+        tgttos: 1330,
+        bb: 1573,
+        hitw: 1503,
+      }
+    }
+  },
+  Kaimaxon: {
+    iw: {
+      2: {
+        sb: 430,
+        tgttos: 946,
+        bb: 1170,
+        hitw: 815,
+      }
+    }
+  },
+  TheDirtyRowlet: {
+    iw: {
+      2: {
+        sb: 489,
+        tgttos: 648,
+        bb: 1188,
+        hitw: 562,
+      }
+    }
+  },
+  Nightmarine: {
+    iw: {
+      2: {
+        sb: 515,
+        tgttos: 1288,
+        bb: 1218,
+        hitw: 927,
+      }
+    }
+  },
+  ori: {
+    it: {
+      2: {
+        sb: 224,
+        tgttos: 469,
+        hitw: 475,
+        bb: 720,
+      }
+    }
+  },
+  Leo: {
+    it: {
+      2: {
+        sb: 269,
+        tgttos: 1179,
+        hitw: 980,
+        bb: 870,
+      }
+    }
+  },
+  Ryu: {
+    it: {
+      2: {
+        sb: 288,
+        tgttos: 721,
+        hitw: 840,
+        bb: 1455,
+      }
+    }
+  },
+  Finbo: {
+    it: {
+      2: {
+        sb: 338,
+        tgttos: 1000,
+        hitw: 935,
+        bb: 1215,
+      }
+    }
+  },
+  Cvenus: {
+    it: {
+      2: {
+        sb: 210,
+        tgttos: 805,
+        hitw: 1125,
+        bb: 1620,
+      }
+    }
+  },
+  envy: {
+    it: {
+      2: {
+        sb: 266,
+        tgttos: 616,
+        hitw: 1640,
+        bb: 1305,
+      }
+    }
+  },
+  sharpestfork: {
+    it: {
+      2: {
+        sb: 666,
+        tgttos: 620,
+        hitw: 530,
+        bb: 2205,
+      }
+    }
+  },
+  Xiler: {
+    it: {
+      2: {
+        sb: 440,
+        tgttos: 1125,
+        hitw: 980,
+        bb: 1815,
+      },
+    },
+    iw: {
+      2: {
+        sb: 91,
+        tgttos: 1362,
+        bb: 772,
+        hitw: 942
+      }
+    }
+  },
+  ITZWBL_: {
+    it: {
+      2: {
+        sb: 589,
+        tgttos: 719,
+        hitw: 1555,
+        bb: 1440,
+      }
+    }
+  },
+  Kingscortcher: {
+    it: {
+      2: {
+        sb: 290,
+        tgttos: 1682,
+        hitw: 1435,
+        bb: 1410,
+      }
+    }
+  },
+  Andante: {
+    it: {
+      2: {
+        sb: 266,
+        tgttos: 1386,
+        hitw: 1600,
+        bb: 1635,
+      }
+    }
+  },
+  Hiitzhunter: {
+    it: {
+      2: {
+        sb: 429,
+        tgttos: 1445,
+        hitw: 2090,
+        bb: 990,
+      }
+    }
+  },
+  sppirit: {
+    it: {
+      2: {
+        sb: 568,
+        tgttos: 1869,
+        hitw: 880,
+        bb: 1620,
+      }
+    }
+  },
+  turtleonMC: {
+    it: {
+      2: {
+        sb: 554,
+        tgttos: 1597,
+        hitw: 1325,
+        bb: 1665,
+      }
+    }
+  },
+  Fu6u: {
+    it: {
+      2: {
+        sb: 266,
+        tgttos: 1938,
+        hitw: 1665,
+        bb: 1455,
+      }
+    }
+  },
+  Clovish: {
+    it: {
+      2: {
+        sb: 378,
+        tgttos: 1536,
+        hitw: 2040,
+        bb: 1350,
+      }
+    }
+  },
+  redralis: {
+    it: {
+      2: {
+        sb: 690,
+        tgttos: 1232,
+        hitw: 1590,
+        bb: 1665,
+      }
+    }
+  },
+  TheHiddenVoid: {
+    it: {
+      2: {
+        sb: 370,
+        tgttos: 2147,
+        hitw: 1575,
+        bb: 1620,
+      }
+    }
+  },
+  Nefasto: {
+    it: {
+      2: {
+        sb: 813,
+        tgttos: 1571,
+        hitw: 1455,
+        bb: 1815,
+      }
+    }
+  },
+  Witherbeast: {
+    it: {
+      2: {
+        sb: 381,
+        tgttos: 2036,
+        hitw: 2075,
+        bb: 1440,
+      }
+    }
+  },
+  Cho: {
+    it: {
+      2: {
+        sb: 909,
+        tgttos: 1876,
+        hitw: 1275,
+        bb: 1755,
+      }
+    }
+  },
+  Erik: {
+    it: {
+      2: {
+        sb: 789,
+        tgttos: 1416,
+        hitw: 1525,
+        bb: 2175,
+      }
+    }
+  },
+  Prythium: {
+    it: {
+      2: {
+        sb: 730,
+        tgttos: 2026,
+        hitw: 1500,
+        bb: 1770,
+      }
+    }
+  },
+  Pika: {
+    it: {
+      2: {
+        sb: 378,
+        tgttos: 1951,
+        hitw: 2425,
+        bb: 1770,
+      }
+    }
+  },
+  KBelik: {
+    it: {
+      2: {
+        sb: 965,
+        tgttos: 1724,
+        hitw: 1700,
+        bb: 1860,
+      }
+    }
+  },
+  forteo: {
+    it: {
+      2: {
+        sb: 1872,
+        tgttos: 1456,
+        hitw: 1385,
+        bb: 2115,
+      }
+    }
+  },
+  kewll: {
+    it: {
+      2: {
+        sb: 1664,
+        tgttos: 1896,
+        hitw: 1860,
+        bb: 2610,
+      }
+    }
+  },
+  Doozim: {
+    iw: {
+      1: {
+        sb: 1448,
+        bb: 2025,
+        tgttos: 1803,
+        hitw: 2085,
+      },
+      2: {
+        sb: 964,
+        bb: 1370,
+        tgttos: 1273,
+        hitw: 1623,
+      }
+    }
+  },
+  Brunted: {
+    iw: {
+      1: {
+        sb: 1212,
+        bb: 1935,
+        tgttos: 1929,
+        hitw: 1750,
+      }
+    }
+  },
+  Rotomm: {
+    it: {
+      2: {
+        sb: 778,
+        tgttos: 1608,
+        hitw: 2160,
+        bb: 1935,
+      }
+    },
+    iw: {
+      1: {
+        sb: 974,
+        bb: 1860,
+        tgttos: 2164,
+        hitw: 1860,
+        sub: true
+      }
+    }
+  },
+  Cautiontape14: {
+    iw: {
+      1: {
+        sb: 974,
+        bb: 1860,
+        tgttos: 2164,
+        hitw: 1860,
+        sub: true
+      }
+    }
+  },
+  qHusky: {
+    iw: {
+      1: {
+        sb: 698,
+        bb: 1845,
+        tgttos: 2064,
+        hitw: 1775,
+      }
+    }
+  },
+  Dunkinlmao: {
+    iw: {
+      1: {
+        sb: 662,
+        bb: 1905,
+        tgttos: 1573,
+        hitw: 1835,
+      }
+    },
+    it: {
+      2: {
+        sb: 733,
+        tgttos: 1695,
+        hitw: 1840,
+        bb: 1680
+      }
+    }
+  },
+  PlutoAndreas: {
+    iw: {
+      1: {
+        sb: 1120,
+        bb: 1440,
+        tgttos: 1652,
+        hitw: 1380,
+        sub: true
+      },
+      2: {
+        sb: 248,
+        tgttos: 1386,
+        bb: 1262,
+        hitw: 1122
+      }
+    }
+  },
+  cxost: {
+    iw: {
+      1: {
+        sb: 1120,
+        bb: 1440,
+        tgttos: 1652,
+        hitw: 1380,
+        sub: true
+      }
+    }
+  },
+  Toremood: {
+    iw: {
+      1: {
+        sb: 242,
+        bb: 1950,
+        tgttos: 1796,
+        hitw: 2035,
+      },
+      2: {
+        sb: 879,
+        tgttos: 1158,
+        bb: 994,
+        hitw: 1445
+      }
+    },
+    it: {
+      2: { 
+        sb: 466,
+        tgttos: 1832,
+        hitw: 1870,
+        bb: 1755
+      }
+    }
+  },
+  Zenrul: {
+    iw: {
+      1: {
+        sb: 732,
+        bb: 1710,
+        tgttos: 1623,
+        hitw: 1695,
+      }
+    }
+  },
+  Dawld: {
+    iw: {
+      1: {
+        sb: 926,
+        bb: 1740,
+        tgttos: 1300,
+        hitw: 1600,
+      }
+    }
+  },
+  Prospect: {
+    iw: {
+      1: {
+        sb: 926,
+        bb: 1740,
+        tgttos: 1300,
+        hitw: 1600,
+      }
+    }
+  },
+  MeanAtoh: {
+    iw: {
+      1: {
+        sb: 868,
+        bb: 1575,
+        tgttos: 1900,
+        hitw: 1210,
+      }
+    }
+  },
+  Chionphobia: {
+    iw: {
+      1: {
+        sb: 376,
+        bb: 1575,
+        tgttos: 1911,
+        hitw: 1930,
+      }
+    }
+  },
+  Jsharps7: {
+    iw: {
+      1: {
+        sb: 608,
+        bb: 1995,
+        tgttos: 1515,
+        hitw: 1555,
+      },
+      2: {
+        sb: 289,
+        tgttos: 1516,
+        bb: 1010,
+        hitw: 1653
+      }
+    }
+  },
+  GrudgingComb: {
+    iw: {
+      1: {
+        sb: 724,
+        bb: 1980,
+        tgttos: 1365,
+        hitw: 1470,
+        sub: true
+      }
+    }
+  },
+  masmarvelous: {
+    iw: {
+      1: {
+        sb: 724,
+        bb: 1980,
+        tgttos: 1365,
+        hitw: 1470,
+        sub: true
+      }
+    }
+  },
+  SomethingNeon: {
+    iw: {
+      1: {
+        sb: 1216,
+        bb: 1815,
+        tgttos: 1082,
+        hitw: 1095,
+      },
+      2: {
+        sb: 701,
+        tgttos: 1492,
+        bb: 1440,
+        hitw: 1882
+      }
+    }
+  },
+  EeveesXM: {
+    iw: {
+      1: {
+        sb: 532,
+        bb: 1845,
+        tgttos: 1456,
+        hitw: 1510,
+      },
+      2: {
+        sb: 554,
+        tgttos: 1022,
+        bb: 1337,
+        hitw: 1551
+      }
+    }
+  },
+  Cookithy: {
+    iw: {
+      1: {
+        sb: 462,
+        bb: 2010,
+        tgttos: 1723,
+        hitw: 1180,
+      }
+    }
+  },
+  Konzid: {
+    iw: {
+      1: {
+        sb: 858,
+        bb: 1320,
+        tgttos: 1202,
+        hitw: 1685,
+      }
+    }
+  },
+  Collroll: {
+    iw: {
+      1: {
+        sb: 796,
+        bb: 1350,
+        tgttos: 1384,
+        hitw: 1495,
+      },
+      2: {
+        sb: 1065,
+        tgttos: 1382,
+        bb: 1470,
+        hitw: 728
+      }
+    }
+  },
+  GeorgeDripington: {
+    iw: {
+      1: {
+        sb: 386,
+        bb: 1782,
+        tgttos: 1420,
+        hitw: 1600,
+      }
+    }
+  },
+  Twility: {
+    iw: {
+      1: {
+        sb: 530,
+        bb: 1573,
+        tgttos: 1416,
+        hitw: 1589,
+      },
+      2: {
+        sb: 350,
+        tgttos: 1250,
+        bb: 1010,
+        hitw: 1492
+      }
+    },
+    it: {
+      2: {
+        sb: 232,
+        tgttos: 1467,
+        hitw: 2105,
+        bb: 1890 
+      }
+    }
+  },
+  vempireAYO: {
+    iw: {
+      1: {
+        sb: 596,
+        bb: 1305,
+        tgttos: 1420,
+        hitw: 1266,
+      },
+      2: {
+        sb: 346,
+        tgttos: 1062,
+        bb: 1143,
+        hitw: 1432
+      }
+    }
+  },
+  Super_Noot: {
+    iw: {
+      1: {
+        sb: 712,
+        bb: 1575,
+        tgttos: 1575,
+        hitw: 1312,
+      }
+    }
+  },
+  ParkerId7: {
+    iw: {
+      1: {
+        sb: 358,
+        bb: 1545,
+        tgttos: 1811,
+        hitw: 1390,
+      }
+    }
+  },
+  Triqary: {
+    iw: {
+      1: {
+        sb: 456,
+        bb: 1770,
+        tgttos: 1360,
+        hitw: 1328,
+      }
+    },
+    it: {
+      2: {
+        sb: 381,
+        tgttos: 1526,
+        hitw: 922,
+        bb: 1050
+      }
+    }
+  },
+  Bafes: {
+    iw: {
+      1: {
+        sb: 808,
+        bb: 1590,
+        tgttos: 1070,
+        hitw: 972,
+      }
+    }
+  },
+  Gree1000: {
+    iw: {
+      1: {
+        sb: 712,
+        bb: 1485,
+        tgttos: 685,
+        hitw: 1594,
+      }
+    }
+  },
+  Spow_: {
+    iw: {
+      1: {
+        sb: 412,
+        bb: 1350,
+        tgttos: 1285,
+        hitw: 1451,
+      }
+    }
+  },
+  Frwzn: {
+    iw: {
+      1: {
+        sb: 276,
+        bb: 1680,
+        tgttos: 930,
+        hitw: 1480,
+      },
+      2: {
+        sb: 426,
+        tgttos: 1461,
+        bb: 1291,
+        hitw: 1244
+      }
+    }
+  },
+  TheWolfJ: {
+    iw: {
+      1: {
+        sb: 436,
+        bb: 1350,
+        tgttos: 1040,
+        hitw: 1243,
+      },
+      2: {
+        sb: 121,
+        tgttos: 961,
+        bb: 1383,
+        hitw: 1244
+      }
+    }
+  },
+  Ruderrr: {
+    iw: {
+      1: {
+        sb: 446,
+        bb: 1170,
+        tgttos: 1240,
+        hitw: 1182,
+        sub: true
+      },
+      2: {
+        sb: 180,
+        tgttos: 1366,
+        bb: 1307,
+        hitw: 980
+      }
+    }
+  },
+  Creekchub: {
+    iw: {
+      1: {
+        sb: 446,
+        bb: 1170,
+        tgttos: 1240,
+        hitw: 1182,
+        sub: true
+      },
+      2: {
+        sb: 311,
+        tgttos: 1330,
+        bb: 1170,
+        hitw: 248
+      }
+    }
+  },
+  Spy_Games: {
+    iw: {
+      1: {
+        sb: 296,
+        bb: 1305,
+        tgttos: 1260,
+        hitw: 966,
+      }
+    }
+  },
+  Xanyu: {
+    iw: {
+      1: {
+        sb: 366,
+        bb: 1170,
+        tgttos: 1250,
+        hitw: 925,
+      },
+      2: {
+        sb: 205,
+        tgttos: 1154,
+        bb: 1132,
+        hitw: 1372
+      }
+    }
+  },
+  Sky2516: {
+    iw: {
+      1: {
+        sb: 258,
+        bb: 1575,
+        tgttos: 805,
+        hitw: 1096,
+      }
+    }
+  },
+  Trash_Panda2007: {
+    iw: {
+      1: {
+        sb: 232,
+        bb: 1695,
+        tgttos: 830,
+        hitw: 943,
+      }
+    }
+  },
+  BubblesTheFriend: {
+    iw: {
+      1: {
+        sb: 138,
+        bb: 1395,
+        tgttos: 1105,
+        hitw: 896,
+      },
+      2: {
+        sb: 117,
+        tgttos: 814,
+        bb: 1278,
+        hitw: 1197
+      }
+    }
+  },
+  DANTCE: {
+    iw: {
+      1: {
+        sb: 0,
+        bb: 1545,
+        tgttos: 455,
+        hitw: 1225,
+      }
+    }
+  },
+  istotalysans: {
+    iw: {
+      1: {
+        sb: 114,
+        bb: 1245,
+        tgttos: 655,
+        hitw: 1015,
+      }
+    }
+  },
+  LoadingSiren597: {
+    iw: {
+      1: {
+        sb: 288,
+        bb: 1170,
+        tgttos: 610,
+        hitw: 818,
+      },
+      2: {
+        sb: 119,
+        tgttos: 430,
+        bb: 683,
+        hitw: 332
+      }
+    }
+  },
+  Deadvid: {
+    iw: {
+      1: {
+        sb: 204,
+        bb: 1170,
+        tgttos: 750,
+        hitw: 645,
+      }
+    }
+  },
+  Torteal: {
+    iw: {
+      1: {
+        sb: 244,
+        bb: 1095,
+        tgttos: 685,
+        hitw: 696,
+      }
+    }
+  },
+  Aurrav: {
+    iw: {
+      2: {
+        sb: 1004,
+        tgttos: 1013,
+        bb: 1440,
+        hitw: 1237,
+      }
+    }
+  },
+  Minelover: {
+    iw: {
+      2: {
+        sb: 847,
+        tgttos: 1790,
+        bb: 1573,
+        hitw: 1870,
+      }
+    }
+  },
+  Umendan: {
+    iw: {
+      2: {
+        sb: 819,
+        tgttos: 1194,
+        bb: 1170,
+        hitw: 1062,
+      }
+    }
+  },
+  NotCM: {
+    iw: {
+      2: {
+        sb: 762,
+        tgttos: 801,
+        bb: 1349,
+        hitw: 1027,
+      }
+    }
+  },
+  Guppless: {
+    iw: {
+      2: {
+        sb: 735,
+        tgttos: 1021,
+        bb: 1307,
+        hitw: 998,
+      }
+    }
+  },
+  Stumberr: {
+    iw: {
+      2: {
+        sb: 623,
+        tgttos: 1110,
+        bb: 1132,
+        hitw: 1083,
+      }
+    }
+  },
+  VoidPT: {
+    iw: {
+      2: {
+        sb: 600,
+        tgttos: 1557,
+        bb: 1027,
+        hitw: 1429,
+      }
+    }
+  },
+  Voidol: {
+    iw: {
+      2: {
+        sb: 576,
+        tgttos: 1594,
+        bb: 1573,
+        hitw: 1716,
+      }
+    }
+  },
+  Sneapky: {
+    iw: {
+      2: {
+        sb: 564,
+        tgttos: 1505,
+        bb: 1604,
+        hitw: 1593,
+      }
+    }
+  },
+  Txciic: {
+    iw: {
+      2: {
+        sb: 548,
+        tgttos: 1218,
+        bb: 1054,
+        hitw: 1664,
+      }
+    }
+  },
+  SuitSnap: {
+    iw: {
+      2: {
+        sb: 323,
+        tgttos: 692,
+        bb: 758,
+        hitw: 643,
+      }
+    }
+  },
+  LeonardoPLK: {
+    iw: {
+      2: {
+        sb: 289,
+        tgttos: 1265,
+        bb: 1084,
+        hitw: 728,
+      }
+    }
+  },
+  DaJamesMayo: {
+    iw: {
+      2: {
+        sb: 168,
+        tgttos: 584,
+        bb: 994,
+        hitw: 1300,
+        sub: true
+      }
+    }
+  },
+}
+
+/*
+
+t: {
+  iw: {
+    2: {
+      sb: ,
+      tgttos: ,
+      bb: ,
+      hitw: ,
+    }
+  }
+},
+
+*/
