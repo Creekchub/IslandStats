6ed4f3a2916fbe1d9631e2b9b68d7ca4b82e3ed2
Creekchub <139086933+Creekchub@users.noreply.github.com>
2023-10-03 13:52:05 -0400

    d
    d

--- players.js
+++ players.js
@@ -3611,7 +3611,7 @@ t: {
 
 */
 
-async function getUsernameToUUID(username) {
+/*async function getUsernameToUUID(username) {
   const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
   const data = await response.json();
   
@@ -3629,4 +3629,4 @@ for(aa = theNum -20; aa < theNum; aa++) {
   let username = `${playersArray[aa]}`;
   getUsernameToUUID(username)
   .then(uuid => console.log(`${username}: ${uuid}`))
-}
+}*/
